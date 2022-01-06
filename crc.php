385879f8b8f716a753b28aef8ac2192b1679f40c
Son Nguyen <sonnn.uct@gmail.com>
2022-01-06 16:12:19 +0700

    init

---
+++ crc.php
@@ -0,0 +1,22 @@
+<?php
+class CRC {
+	public static function CheckSum($str) {
+		$crc = 0xFFFF;
+		$strlen = strlen($str);
+		for($c = 0; $c < $strlen; $c++) {
+			$crc ^= ord(substr($str, $c)) << 8;
+			for($i = 0; $i < 8; $i++) {
+				if($crc & 0x8000) {
+					$crc = ($crc << 1) ^ 0x1021;
+				} else {
+					$crc = $crc << 1;
+				}
+			}
+		}
+		$hex = $crc & 0xFFFF;
+		$hex = dechex($hex);
+		$hex = strtoupper($hex);
+
+		return $hex;
+	}
+}
